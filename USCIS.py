9d89a15c180544bf0926e4e623840c22352f96c2
DJM <dustinmeckley@gmail.com>
2019-04-06 16:06:37 -0400

    Created a directory for resources and added the study guides taken from the USCIS website to the resources for future reference.

--- USCIS.py
+++ USCIS.py
@@ -133,7 +133,7 @@ def getQuestion(number: int) -> str:
         }
     return(questions.get(number))
      
-def getAnswers(number: int) -> str:
+def getAnswers(number: int):
     answers = {
         1: "The Constitution",
         2: ["Sets up the government", 
